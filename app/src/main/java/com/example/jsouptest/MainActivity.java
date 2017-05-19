75dab1b2d7615a81db0b99a7be193a26fef75a7f
AaronPasi <linpx.jack@gmail.com>
2017-05-19 12:04:27 +0800

    修改网页改版导致新闻简介获取不到

--- app/src/main/java/com/example/jsouptest/MainActivity.java
+++ app/src/main/java/com/example/jsouptest/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -63,14 +64,15 @@ public class MainActivity extends AppCompatActivity {
 
                         Document doc = Jsoup.connect("https://voice.hupu.com/nba/" + Integer.toString(i)).get();
                         Elements titleLinks = doc.select("div.list-hd");    //解析来获取每条新闻的标题与链接地址
-                        Elements descLinks = doc.select("div.list-content");//解析来获取每条新闻的简介
+                     // Elements descLinks = doc.select("div.list-content");//解析来获取每条新闻的简介
                         Elements timeLinks = doc.select("div.otherInfo");   //解析来获取每条新闻的时间与来源
+                        Log.e("title",Integer.toString(titleLinks.size()));
                         for(int j = 0;j < titleLinks.size();j++){
                             String title = titleLinks.get(j).select("a").text();
                             String uri = titleLinks.get(j).select("a").attr("href");
-                            String desc = descLinks.get(j).select("span").text();
+                       //   String desc = descLinks.get(j).select("span").text();
                             String time = timeLinks.get(j).select("span.other-left").select("a").text();
-                            News news = new News(title,uri,desc,time);
+                            News news = new News(title,uri,null,time);
                             newsList.add(news);
                         }
                     }
